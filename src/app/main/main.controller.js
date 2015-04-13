2b7d2f5a3d7ad44f807bcc249b7223bf7bfe08b7
Erik Hellenbrand <Erik@eriks-mbp.attlocal.net>
2015-04-13 02:53:45 -0700

    Add initial styles, markup, and fake data

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -6,10 +6,23 @@ angular.module('redditDeck')
     // TODO (Erik Hellenbrand) : Move reddit resource logic to a service/factory
     $scope.baseUrl = 'http://www.reddit.com/r/';
     $scope.subs = [];
+    $scope.links = [
+      {
+        displayText: 'First link goes here',
+        description: 'Maybe a little preview goes here?',
+        url: 'http://www.erikhellenbrand.com'
+      },
+
+      {
+        displayText: '2nd link goes here',
+        description: 'Can we even show a preview?',
+        url: 'http://www.google.com'
+      }
+    ];
 
     function getSubsUrl(subsArray) {
       return subsArray.join('+');
     }
 
-    $scope.Reddit = $resource($scope.baseUrl + getSubsUrl($scope.subs));
+    // $scope.Reddit = $resource($scope.baseUrl + getSubsUrl($scope.subs));
   });
