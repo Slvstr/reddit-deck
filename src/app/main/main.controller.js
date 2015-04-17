f762a7e75eb0fca364d1c279affe20b40f1e13c9
Erik Hellenbrand <Erik@eriks-mbp.attlocal.net>
2015-04-17 02:44:50 -0700

    Add stubs for adding, removing, and merging boards

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,7 +3,21 @@
 angular.module('redditDeck')
   .controller('MainCtrl', function ($scope, Storage) {
 
+    // Get boards array from local storage
     $scope.boards = JSON.parse(Storage.getItem('boards'));
+
+    $scope.addBoard = function(index) {
+      // add a new board object to boards array
+    };
+
+
+    $scope.removeBoard = function(index1) {
+      // remove a board from boards array
+    };
+
+    $scope.mergeBoards = function(index1, index2) {
+      // merge 2 boards into a single board by combining subs
+    };
 
 
   });
