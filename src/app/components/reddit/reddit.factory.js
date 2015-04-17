da7af1a701cdbfe3b4756bbe64faba7d8be47c57
Erik Hellenbrand <Erik@eriks-mbp.attlocal.net>
2015-04-16 21:15:10 -0700

    Move reddit logic into service

---
+++ src/app/components/reddit/reddit.factory.js
@@ -0,0 +1,44 @@
+(function() {
+  'use strict';
+
+  angular.module('redditDeck')
+
+  .factory('Reddit', function($http){
+    var _baseUrl = 'http://api.reddit.com/r/';
+    var POSTS_PER_PAGE = 10;
+
+    function getSubsUrl(subsArray) {
+      return subsArray.join('+');
+    }
+
+
+    return function(subs) {
+      var _items = [];
+      var _after = '';
+      var _busy = '';
+      var _url = _baseUrl
+              + getSubsUrl(subs)
+              + '?after='
+              + _after
+              + '&limit=' + POSTS_PER_PAGE
+              + '&jsonp=JSON_CALLBACK';
+
+
+      return {
+        after: _after,
+        getNextPage: function() {
+          return $http.jsonp(_url).success(function(data) {
+            var items = data.data.children;
+            for (var i = 0; i < items.length; i++) {
+              _items.push(items[i].data);
+            }
+            _after = 't3_' + _items[_items.length - 1].id;
+            _busy = false;
+          });
+        },
+        items: _items
+      };
+    };
+  });
+
+})();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,35 +1,14 @@
 'use strict';
 
 angular.module('redditDeck')
-  .controller('MainCtrl', function ($scope, $http) {
-
-    // TODO (Erik Hellenbrand) : Move reddit resource logic to a service/factory
-    this.baseUrl = 'http://api.reddit.com/r/';
-    this.after = '';
+  .controller('MainCtrl', function ($scope, Reddit) {
 
     $scope.subs = ['news'];
-    this.items = [];
-    $scope.items = this.items;
-    
-    function getSubsUrl(subsArray) {
-      return subsArray.join('+');
-    }
+    var reddit = Reddit($scope.subs);
 
-    this.url = this.baseUrl
-              + getSubsUrl($scope.subs)
-              + '?after='
-              + this.after
-              + '&limit=10'
-              + '&jsonp=JSON_CALLBACK';
+    $scope.items = reddit.items;
 
-    $http.jsonp(this.url).success(function(data) {
-      var items = data.data.children;
-      for (var i = 0; i < items.length; i++) {
-        this.items.push(items[i].data);
-      }
-      console.dir(this.items);
-      this.after = "t3_" + this.items[this.items.length - 1].id;
-      this.busy = false;
-    }.bind(this));
 
+    reddit.getNextPage();
+  
   });
